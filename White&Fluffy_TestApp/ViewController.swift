db632c2ab9691a403677488633866e6fcb068eae
Sergey-Yurtaev <engineerk2@mail.ru>
2022-09-06 15:05:48 +0500

    Initial Commit

---
+++ White&Fluffy_TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  White&Fluffy_TestApp
+//
+//  Created by  Sergey Yurtaev on 06.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
